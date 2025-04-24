09842c11c1d860a621dd4c33e23dbb18457949fa
neivany1 <nevilenaivanny@gmail.com>
2025-04-24 15:36:48 +0300

    Initialized laravel app

--- routes/api.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\WeatherApiController;
-
-Route::get('/weather/current', [WeatherApiController::class, 'getCurrentWeather']);
-Route::get('/weather/forecast', [WeatherApiController::class, 'getForecast']);
-Route::get('/weather/geocode', [WeatherApiController::class, 'geocodeCity']);
